c2db528b6bd05c5e3342ca12b13aa0b4b34f7b46
AmazingMorbid <tomek.niezurawski@gmail.com>
2021-05-20 14:14:08 +0200

    Changed limits

--- api/v1/device_recognizer.py
+++ api/v1/device_recognizer.py
@@ -18,7 +18,7 @@ def index():
 
 @router.post("/get-info", response_model=DeviceInfoResponse)
 def get_device_info(texts: DeviceInfoRequest):
-    if len(texts.texts) > 50:
-        raise HTTPException(status_code=422, detail="You cannot process more than 50 texts at a time.")
+    if len(texts.texts) > 500:
+        raise HTTPException(status_code=422, detail="You cannot process more than 500 texts at a time.")
 
     return DeviceInfoResponse(info=list(device_recognizer.get_info(texts.texts)))
